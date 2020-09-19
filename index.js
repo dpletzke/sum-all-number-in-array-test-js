c657a6e6c9511567a68ed0feb603dc660b0f5acb
Dan Pletzke <dpletzke@gmail.com>
2020-09-19 18:14:24 +0000

    initial array sum solution

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  /* three conditions, if 
+  is array =>
+  is num => increment i pass
+  doesn't not exist  */
+
+  let acc = 0;
+  for(e of array) {
+    if(Array.isArray(e)) {
+      acc += sumItems(e);
+    }
+    else if (Number.isInteger(e)){
+      acc += e;
+    }   
+  }
+  return acc;
+
+
+
+
+  // if(Array.isArray(array)) {
+  //   return sumItems(array);
+  // }
+  // else if(array.length) {
+  //   return array[0] + sumItems(array[1]);
+  // }
+  // return 0;
 }
 
 module.exports = sumItems;
